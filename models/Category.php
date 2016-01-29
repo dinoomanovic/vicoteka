d9a58574cdb311a7d10c456594c5fd9cbf6dd5fa
Dino Omanovic <dino.omanovic.15@gmail.com>
2016-01-29 11:17:34 +0100

    update kategorije
    update kategorije

--- models/Category.php
+++ models/Category.php
@@ -7,6 +7,26 @@ use yii\db\ActiveRecord;
  */
 class Category extends ActiveRecord
 {
+  public static function tableName()
+  {
+    return 'category';
+  }
+public function rules()
+{
+  return [
+      [['name'], 'required', 'message' => 'Morate unijeti tražene podatke'],
+      [['categoryID'], 'integer'],
+      [['name'], 'string']
+    ];
+      }
+      public function attributeLabels()
+      {
+          return [
+              'categoryID' => 'ID kategorije',
+              'name' => 'Naziv kategorije',
+];
+}
+
 
 }
 
